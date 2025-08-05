2c606e1945181fcd5778f5242f435e5c65ac4ddd
nanayeb34 <nanayeb34@gmail.com>
2025-08-05 02:31:31 +0000

    modified project root path

--- data/prepare.py
+++ data/prepare.py
@@ -22,7 +22,7 @@ LOG = structlog.stdlib.get_logger()
 os.environ["TOKENIZERS_PARALLELISM"] = "true"
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-project_root = os.path.dirname(os.path.dirname(current_dir))
+project_root =  os.path.dirname(current_dir)
 
 config_path = os.path.join(project_root, "config", "config.yaml")
 config = OmegaConf.load(config_path)
